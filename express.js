9fd7f006ddc9d4e5b306ff08a01846c3af27783f
Rick Kar <150364149+rick-instient@users.noreply.github.com>
2023-12-03 12:14:52 +0530

    Update express.js

--- express.js
+++ express.js
@@ -11,22 +11,26 @@ app.use((req, res, next) => {
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rickkarinstient:VslUlwPfAnJhUqSb@cluster0.si3pzbr.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://rickkarinstient:VslUlwPfAnJhUqSb@cluster0.si3pzbr.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+
+const client = new MongoClient(uri);
 
 async function postData() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        let response = await client.db("rick").collection("users").insertOne({ name: "Rick", role: "owner" });
+        // let response = await client.db("rick").collection("users").insertOne({ name: "Rick", role: "owner" });
+        let response = await client.db('first').collection('mydb').insertOne({ name: "Rick", role: "owner" });
         console.log(response);
         return response;
     } finally {
@@ -40,7 +44,8 @@ async function getData() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        let response = await client.db("rick").collection("users").find().toArray();
+        // let response = await client.db("rick").collection("users").find().toArray();
+        let response = await client.db('first').collection('mydb').find().toArray();
         console.log(response);
         return response;
     } finally {
